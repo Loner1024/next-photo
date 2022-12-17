d6160624866b6ea9245ec08794b05af6112d2b7a
Loner1024 <xth12138@gmail.com>
2022-12-17 23:36:04 +0800

    first commit

---
+++ components/Slider.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {SliderData} from "./SliderData";
+import Image from "next/image";
+
+
+const Slider = ({sliders}: { sliders: SliderData[] }) => {
+    return (
+        <div id="gallery">
+            <h1>Gallery</h1>
+            {sliders.map((slider: SliderData, index: number) => {
+                return (
+                    <Image src={slider.image} alt="/" width="1440" height="600" style={{objectFit: "cover"}}/>
+                )
+            })}
+        </div>
+    );
+};
+
+export default Slider;
---
+++ components/SliderData.ts
@@ -0,0 +1,15 @@
+export type SliderData = {
+    image: string
+}
+
+export const sliderData: SliderData[] = [
+    {
+        image: "https://images.unsplash.com/photo-1669325513580-dfcd4e0ad564?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3432&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1448376561459-dbe8868fa34c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1670837301612-492633adb05c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+    }
+]
